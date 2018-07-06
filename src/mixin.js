67bf42cca86355a586877635118251d51ef439a4
myronliu347 <myronliu347@gmail.com>
2018-07-06 17:14:39 +0800

    fix: arrary

--- src/mixin.js
+++ src/mixin.js
@@ -10,14 +10,21 @@ export default {
     let merge = {};
     switch (true) {
       case typeof options.connect === 'function':
-        merge = options.connect((...args) => museModel.getModelMap(...args));
+        merge = options.connect((...args) => museModel.getModelMap(...args), model => {
+          const actions = {};
+          Object.keys(model).forEach(key => {
+            if (typeof model[key] !== 'function') return;
+            actions[key] = model[key];
+          });
+          return actions;
+        });
         break;
       case Array.isArray(options.connect):
         const maps = museModel.getModelMap.apply(museModel, options.connect);
         merge.computed = {};
         merge.methods = {};
-        Object.keys(maps).forEach((map) => {
-          const options = getMergeOptions(map);
+        Object.keys(maps).forEach((key) => {
+          const options = getMergeOptions(maps[key]);
           merge.computed = {
             ...merge.computed,
             ...options.computed
